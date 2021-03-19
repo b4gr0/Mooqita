3514513ccbd5d657aff6f1917afd335ea45bb2f7
b4gr0 <71821419+b4gr0@users.noreply.github.com>
2021-03-19 16:42:22 +0100

    Update Mooqita_Linux_challenge_beginner.sh

--- Mooqita_Linux_challenge_beginner.sh
+++ Mooqita_Linux_challenge_beginner.sh
@@ -7,7 +7,7 @@ CLEARED_FILE=$TEMP_FILE.cleared
 # while loop controling size of created file
 while [ $(stat -c%s $TEMP_FILE) -lt $((1024*1024)) ]; do
     cat /dev/urandom | tr -dc  [:alnum:] | fold -w 15 | head -n $((1024)) >> $TEMP_FILE
-# too speed up process of file creation, the buffer of 15*1024 bytes was used utlizing head command
+# to speed up process of file creation, the buffer of 15*1024 bytes was used utlizing head command
 done
 echo The size of generated file \($TEMP_FILE\) is: $(stat -c%s $TEMP_FILE) bytes
 
